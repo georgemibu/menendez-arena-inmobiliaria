d618728e0e087f19b230b43d9070c95c67837dd9
georgemibu <georgemibu@gmail.com>
2026-05-08 12:10:06 -0300

    arranco proyecto

---
+++ script.js
@@ -0,0 +1,13 @@
+
+new Swiper(".galeria-mobile", {
+
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+
+});
+
