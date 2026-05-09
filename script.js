5b46d402a1aa5b58d9b1f13686bfc63a7e621353
georgemibu <georgemibu@gmail.com>
2026-05-09 18:19:50 -0300

    arranco proyecto

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ new Swiper(".galeria-mobile", {
 
 });
 
+const hamburger = document.getElementById("hamburger");
+
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+
