d3e9d4533a9ff2cfab9c11a71381799f0614d6c7
georgemibu <georgemibu@gmail.com>
2026-05-11 11:49:23 -0300

    arranco proyecto

--- script.js
+++ script.js
@@ -22,3 +22,56 @@ hamburger.addEventListener("click", () => {
 });
 
 
+// Inicializar EmailJS
+emailjs.init("jrslXejCzh2SMVsPe");
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    const templateParams = {
+        nombre: form.querySelector('[name="nombre"]').value,
+        email: form.querySelector('[name="email"]').value,
+        telefono: form.querySelector('[name="telefono"]').value,
+        proyecto: form.querySelector('[name="proyecto"]').value,
+        zona: form.querySelector('[name="zona"]').value,
+        mensaje: form.querySelector('[name="mensaje"]').value
+    };
+
+    console.log(templateParams);
+
+    emailjs.send(
+        "service_dw8j612",
+        "template_55y1agb",
+        templateParams
+    )
+    .then(() => {
+
+        document.getElementById("popupMensaje").style.display = "flex";
+
+        form.reset();
+
+    })
+    .catch((error) => {
+
+        console.log(error);
+
+        alert("Error al enviar.");
+
+    });
+
+});
+
+
+function cerrarPopup() {
+    document.getElementById("popupMensaje").style.display = "none";
+}
+
+const data = new FormData(form);
+
+for (let pair of data.entries()) {
+    console.log(pair[0], pair[1]);
+}
+
